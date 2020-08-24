084a9e74a60f87bcce4842f6588fd83f6ed1d34a
BOLNUT <bolnut3@gmail.com>
2020-08-24 18:14:10 +0900

    rootの指定
    ログイン画面に遷移してしまうため
    authenticate_user!をコメントに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! # ログインしていないユーザーをサインアップの画面に促す
+  # before_action :authenticate_user! # ログインしていないユーザーをサインアップの画面に促す
 end
