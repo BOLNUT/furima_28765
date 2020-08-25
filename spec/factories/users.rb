c0445166e6b3852a1d7898d2a0c8b3dee641d987
BOLNUT <bolnut3@gmail.com>
2020-08-25 19:35:23 +0900

    factorybotの記載(仮

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nick_name              {"test"}
+    nick_name             {"test"}
     email                 {"kkk@gmail.com"}
     password              {"00000000"}
     password_confirmation {password}
+    family_name           {"田中"}
+    first_name            {"太郎"}
+    family_name_kana      {"タナカ"}
+    first_name_kana       {"タロウ"}
+    birth_date            {""}
   end
 end
