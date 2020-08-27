10d037bde9ff9362962a45a28b0fd0e524e9827f
BOLNUT <bolnut3@gmail.com>
2020-08-27 17:10:54 +0900

    createアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,16 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   
+  def create
+    Item.create(item_params) #モデル名.create(引数名)
+  end
+   
+   private
+    def item_params
+      params.require(:items).permit(
+        :image, :name, :text, :price, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :time_id, :user_id
+      ) #active_hash使用       :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :time_id  外部キー:user_id
+    end
+   
 end
 
