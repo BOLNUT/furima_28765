14f281f43cd0854baca5953413ed6eff84d454bb
BOLNUT <bolnut3@gmail.com>
2020-08-28 16:45:12 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,8 @@ class ItemsController < ApplicationController
     @discrimination = Discrimination.new
     @status = Status.new
     @fee = Fee.new
+    @address = Address.new
+    @delivery_time = DeliveryTime.new
     #//new active_hash使用
   end
   
@@ -38,6 +40,21 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+    @delivery_time = DeliveryTime.new(delivery_time_params)
+    if @delivery_time.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
     #//create active_hash使用
   end
    
@@ -58,6 +75,14 @@ class ItemsController < ApplicationController
 
     def fee_params
       params.require(:fee).permit(:title,:text,:delivery_fee_id)
+    end
+
+    def address_params
+      params.require(:address).permit(:title,:text,:prefecture_id)
+    end
+
+    def delivery_time_params
+      params.require(:delivery_time).permit(:title,:text,:delivery_day_id)
     end
     #//params active_hash使用
 end
