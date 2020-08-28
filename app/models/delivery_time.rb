276bee54c1472024146ef963469ef6bbcc72f7c2
BOLNUT <bolnut3@gmail.com>
2020-08-28 15:33:22 +0900

    モデルの設定

--- app/models/delivery_time.rb
+++ app/models/delivery_time.rb
@@ -1,2 +1,10 @@
 class DeliveryTime < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery_day
+
+  #空文字制限
+  validates :title, :text, :delivery_day, presence: true
+
+  #ジャンルの選択が「--」の時は保存できない
+  validates :delivery_day_id, numericality: { other_than: 1 } 
 end
