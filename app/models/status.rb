62e68d853ae0c7d57770f7d9f4175725e6292211
BOLNUT <bolnut3@gmail.com>
2020-08-28 13:18:02 +0900

    バリデーションとカラム名追加

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,10 @@
 class Status < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_status
+
+  #空文字制限
+  validates :title, :text, :category, presence: true
+
+  #ジャンルの選択が「--」の時は保存できない
+  validates :category_id, numericality: { other_than: 1 } 
 end
--- db/migrate/20200828041214_create_statuses.rb
+++ db/migrate/20200828041214_create_statuses.rb
@@ -1,7 +1,9 @@
 class CreateStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :statuses do |t|
-
+      t.string     :title        , null: false
+      t.text       :text         , null: false
+      t.integer    :category_id  , null: false
       t.timestamps
     end
   end
