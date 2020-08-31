373e9f84f3e428eef380a8589a7b80e876e033dd
BOLNUT <bolnut3@gmail.com>
2020-08-31 14:43:52 +0900

    エラー文の編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,62 +15,62 @@ RSpec.describe Item, type: :model do
       it "画像がないとツイートは保存できない" do
         @item.image = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品画像を入力してください")
       end
       it "商品名がないとツイートは保存できない" do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it "説明文がないとツイートは保存できない" do
         @item.text = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品の説明文を入力してください")
       end
       it "カテゴリーが未選択だとツイートは保存できない" do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("カテゴリーは1以外の値にしてください")
       end
       it "商品の状態が未選択だと保存できない" do
         @item.item_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品の状態は1以外の値にしてください")
       end
       it "配送料の負担が未選択だと保存できない" do
         @item.delivery_fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("配送料の負担は1以外の値にしてください")
       end
       it "発送元の地域が未選択だと保存できない" do
-        @item.prefecture = 1
+        @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("発送元の地域は1以外の値にしてください")
       end
       it "発送までの日数が未選択だと保存できない" do
         @item.delivery_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("発送までの日数は1以外の値にしてください")
       end
       it "価格を入力していないと保存できない" do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品価格を入力してください", "商品価格は不正な値です", "商品価格は数値で入力してください")
       end
       it "価格が300円未満だと保存できない" do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include("商品価格は300以上の値にしてください")
       end
       it "価格が9,999,999円を超えると保存できない" do
         @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("image")
+        expect(@item.errors.full_messages).to include()
       end     
       it "ユーザーが紐付いていないとツイートは保存できない" do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
