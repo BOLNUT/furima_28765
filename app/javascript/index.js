41df064eeb06440fe462af601fb6832e43922722
BOLNUT <bolnut3@gmail.com>
2020-09-01 15:07:33 +0900

    js編集

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function(){
   
-  var addFee = document.getElementById("item-price")
-  addFee.addEventListener('onchange', function(){
-    var valueAddFee = document.getElementById("item-price").value
-    var Fee = parseInt(valueAddFee, 10) * (0.1)
-    var Profit = parseInt(valueAddFee, 10) - (Fee)
+  const priceWindow = document.getElementById("item-price")
+  priceWindow.addEventListener('change', function(){
+    var price = priceWindow.value
+    var Fee = parseInt(price, 10) * (0.1)
+    var Profit = parseInt(price, 10) - (Fee)
     document.getElementById("add-tax-price").innerHTML = String(Fee)
     document.getElementById("profit").innerHTML = String(Profit)
-})
+  })
 }) 
