c4f6a4ddf44e72f74f2e7a9302de01ffa432b6a3
BOLNUT <bolnut3@gmail.com>
2020-09-03 15:12:28 +0900

    set_item
    destroyとupdateも追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
-  before_action :set_item, only: [:show, :edit]
-
+  before_action :set_item, only: [:show, :edit, :destroy, :update]
+  
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -24,8 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy
     redirect_to root_path
   end
 
@@ -33,9 +32,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item.update(item_params)
+    redirect_to item_path(@item.id)
   end
    
    private
