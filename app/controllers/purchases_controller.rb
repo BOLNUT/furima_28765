46673206c284b92d8ad1d173c4ef3a93d5f67d65
BOLNUT <bolnut3@gmail.com>
2020-09-07 15:21:26 +0900

    ビューの変更
    アクティブハッシュの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
 
   def index
+    @item = Item.find(params[:id])
   end
 
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,8 @@ class Purchase < ApplicationRecord
   has_one :receiver
   belongs_to :item
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  
 end
